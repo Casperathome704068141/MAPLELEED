9053cd1deddee7e8909d4da6ac787c2e6000e221
Anthony Agaba <anthonyluci69@gmail.com>
2025-09-20 14:09:22 +0000

    they are not labled hero1. use these names

--- src/lib/marketing-assets.ts
+++ src/lib/marketing-assets.ts
@@ -9,26 +9,26 @@ export type MarketingAssets = {
 
 export const DEFAULT_MARKETING_ASSETS: MarketingAssets = {
   hero: [
-    '/hero-1.jpg',
-    '/hero-2.jpg',
+    '/pexels-s-a-1620480-10850900.jpg',
+    '/pexels-arpan-parikh-78161269-8850997.jpg',
   ],
   study: [
-    '/study-1.jpg',
-    '/study-2.jpg',
-    '/study-3.jpg',
+    '/pexels-safa-shafeek-754618105-28786993.jpg',
+    '/pexels-andre-furtado-43594-2777934.jpg',
+    '/pexels-christian-heitz-285904-842711.jpg',
   ],
   travel: [
-    '/travel-1.jpg',
-    '/travel-2.jpg',
-    '/travel-3.jpg',
+    '/pexels-ahmetyuksek-33452677.jpg',
+    '/pexels-aj4xo-28135725.jpg',
+    '/pexels-the-ahnafpiash-29453805.jpg',
   ],
   team: [
-    '/team-1.jpg',
-    '/team-2.jpg',
+    '/pexels-baljinder-singh-112079620-33935326.jpg',
+    '/pexels-mian-rizwan-250562-1771256.jpg',
   ],
   gallery: [
-    '/gallery-1.jpg',
-    '/gallery-2.jpg',
-    '/gallery-3.jpg',
+    '/pexels-francesco-ungaro-1525041.jpg',
+    '/pexels-maxfrancis-2246476.jpg',
+    '/pexels-najam-ahmed-1620956-33919918.jpg',
   ],
 };
