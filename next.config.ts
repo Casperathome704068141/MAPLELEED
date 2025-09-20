e8f23944afd9eb824bba4262320683d41f0b04c7
Anthony Agaba <anthonyluci69@gmail.com>
2025-09-20 14:06:45 +0000

    i have added the images i want to be used across the site in the public

--- next.config.ts
+++ next.config.ts
@@ -18,18 +18,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
